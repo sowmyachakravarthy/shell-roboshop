4f8402918f6ddba76ede92cfc2ed23329fc0edf2
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-24 11:22:08 +0530

    conversation

---
+++ roboshop.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-00205277be43d1520"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "user" "shipping" "cart" "payment" "catalogue" 
+"dispatch" "frontend")
+ZONE_ID="Z00892532APZT7EC7Z6FX"
+DOMAIN_NAME="robolearning.site"
+
+for instance in ${INSTANCES[@]}
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" 
+    --query "Instances[0].InstanceId" --output text)
+
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+        echo "$instance IP address: $IP"
+    fi 
+
+    echo "$instance IP address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
+done
