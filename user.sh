fa9dbfd19efb09dd313ac8bd77c706733e56512e
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-24 16:15:47 +0530

    conversation

--- user.sh
+++ user.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -73,4 +74,9 @@ VALIDATE $? "COpying user service"
 systemctl daemon-reload
 systemctl enable user 
 systemctl start user
-VALIDATE $? "Reloading, enabling and restarting the services"
+VALIDATE $? "Reloading, enabling and restarting the services"   
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script execution completed successfully, $Y time taken : $TOTAL_TIME $N" | tee -a $LOG_FILE
